e3a4aef4a3931d7b3c4a8089d7eb60a2acd2ba9d
Nickolazz1 <elanir358@gmail.com>
2020-02-25 17:05:36 +0200

    sqlite3 db api testing

---
+++ sqlite/less1.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+connection = sqlite3.connect('tutorial.db')
+c = connection.cursor()
+
+def create_table():
+    c.execute('CREATE TABLE IF NOT EXISTS stuffToPlot(unix REAL, datestamp TEXT, keyword TEXT, value REAL)')
+
+def data_entry():
+    c.execute("INSERT INTO stuffToPlot VALUES(1234, '2016-01-01', 'Python', 5)")
+    connection.commit()
+    c.close()
+    connection.close()
+
+
+create_table()
+data_entry()
