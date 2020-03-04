be7341d87c214031e95def97c33a2b99ede28ad0
Nickolazz1 <elanir358@gmail.com>
2020-03-04 16:05:42 +0200

    graph representation complete

--- olympiad/TransferTask/transferTask.py
+++ olympiad/TransferTask/transferTask.py
@@ -1,61 +1,63 @@
-# graphs representation in progress
+# graphs representation complete
 
-file = open('data.txt', 'r')
-temp = file.read()
-data = []
+def loadDataString():
+	file = open('data.txt', 'r')
+	data = []
+	for line in file:
+		data.append(line)
+	return data
 
-for c in temp:
-	try:
-		data.append(int(c))
-	except:
-		pass
+def loadDataArray():
+	file = open('data.txt', 'r')
+	temp = file.read()
+	data = [] # [3, 4, 0, 1, 1, 2, 4, 0, 1, 4, 4, 3, 3, 5, 2, 3]
+	for c in temp:
+		try:
+			data.append(int(c))
+		except:
+			pass
+	return data
 
-print(data)
-print("     ")
-# [3, 4, 0, 1, 1, 2, 4, 0, 1, 4, 4, 3, 3, 5, 2, 3]
+def makeGraph(data, datastr):
+	rnum = max(data) + 1
+	T =	[[ 0 for j in range(rnum)] for i in range(rnum)]
+	temp = []
 
-##     0 1 2 3 4 5
-##     a b c d e f
-##    [0 1 0 0 1 0] a 0
-##    [1 0 1 0 1 0] b 1
-##    [0 1 0 1 0 0] c 2
-##    [0 0 1 0 1 1] d 3
-##    [1 1 0 1 0 0] e 4
-##    [0 0 0 1 0 0] f 5
+	for i in range(2, len(datastr)):
+		t = datastr[i]
+		for c in t:
+			try:
+				temp.append(int(c))
+			except:
+				pass
+		T[temp[0]][temp[1]] = 1
+		T[temp[1]][temp[0]] = 1
+		temp = []
 
-
-#     0 1 2 3 4 5
-#     a b c d e f
-T = [[0,0,0,0,0,0], # a 0
-	 [0,0,0,0,0,0], # b 1
-	 [0,0,0,0,0,0], # c 2
-	 [0,0,0,0,0,0], # d 3
-	 [0,0,0,0,0,0], # e 4
-	 [0,0,0,0,0,0]] # f 5
-
-T[0][1] = 1
-T[1][0] = 1
-
-T[1][2] = 1
-T[2][1] = 1
-
-T[4][0] = 1
-T[0][4] = 1
-
-T[1][4] = 1
-T[4][1] = 1
-
-T[4][3] = 1
-T[3][4] = 1
-
-T[3][5] = 1
-T[5][3] = 1
-
-T[2][3] = 1
-T[3][2] = 1
-
-for i in T:
-	print(i)
+	return T
+	##     0 1 2 3 4 5
+	##     a b c d e f
+	##    [0 1 0 0 1 0] a 0
+	##    [1 0 1 0 1 0] b 1
+	##    [0 1 0 1 0 0] c 2
+	##    [0 0 1 0 1 1] d 3
+	##    [1 1 0 1 0 0] e 4
+	##    [0 0 0 1 0 0] f 5
 
 
+def solution():
+	data = loadDataArray()
+	datastr = loadDataString()
+	start = data[0]
+	finish = data[1]
+	G = makeGraph(data, datastr)
 
+	for i in G:
+		print(i)
+
+	print(start, finish)
+
+
+
+if __name__ == "__main__":
+	solution()
