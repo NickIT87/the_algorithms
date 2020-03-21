a8945849124943f860f78f29c3fb5c18a660f0e7
Nickolazz1 <elanir358@gmail.com>
2020-03-21 21:20:42 +0200

    flask thinking...

--- flask/index.py
+++ flask/index.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    return "Hi there!"
+    return render_template("main.html", name="home")
 
 if __name__ == "__main__":
     app.run()
