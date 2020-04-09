c84989429b9be0f9bab9503809fc5d44c904686e
Nickolazz1 <elanir358@gmail.com>
2020-04-09 19:03:33 +0300

    blog added

--- flask-tutorial/flaskr/__init__.py
+++ flask-tutorial/flaskr/__init__.py
@@ -28,14 +28,14 @@ def create_app(test_config=None):
     def hello():
         return 'Hello, World!'
 
-    @app.route('/')
-    def index():
-        return 'test'
-
     from . import db
     db.init_app(app)
 
     from . import auth
     app.register_blueprint(auth.bp)
+
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
 
     return app
