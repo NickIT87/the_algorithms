83586bafd60e40acdcc4cae033d79800ea1f764c
Nickolazz1 <elanir358@gmail.com>
2020-04-14 17:54:33 +0300

    sentdex socket overview less2

--- async/lesson_2/client.py
+++ async/lesson_2/client.py
@@ -1,13 +1,26 @@
 import socket
 
+HEADERSIZE = 10
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((socket.gethostname(), 1234))
 
-full_msg = ''
 while True:
-    msg = s.recv(8)
-    if len(msg) <= 0:
-        break
-    full_msg += msg.decode("utf-8")
+    full_msg = ''
+    new_msg = True
+    while True:
+        msg = s.recv(16)
+        if new_msg:
+            print(f"new message length: {msg[:HEADERSIZE]}")
+            msglen = int(msg[:HEADERSIZE])
+            new_msg = False
 
-print(full_msg)
+        full_msg += msg.decode("utf-8")
+
+        if len(full_msg)-HEADERSIZE == msglen:
+            print("full msg recvd")
+            print(full_msg[HEADERSIZE:])
+            new_msg = True
+            full_msg = ''
+
+    print(full_msg)
--- async/lesson_2/server.py
+++ async/lesson_2/server.py
@@ -1,10 +1,8 @@
 import socket
+import time
 
-msg = "Welcome to the server!"
-print(f'{len(msg):<20}'+msg)
+HEADERSIZE = 10
 
-
-'''
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((socket.gethostname(), 1234))
 s.listen(5)
@@ -12,6 +10,14 @@ s.listen(5)
 while True:
     clientsocket, address = s.accept()
     print(f"Connection from {address} has been established!")
-    clientsocket.send(bytes("Welcome to the server!", "utf-8"))
-    clientsocket.close()
-'''
+
+    msg = "Welcome to the server!"
+    msg = f'{len(msg):<{HEADERSIZE}}' + msg
+
+    clientsocket.send(bytes(msg, "utf-8"))
+
+    while True:
+        time.sleep(3)
+        msg = f"The time is! {time.time()}"
+        msg = f'{len(msg):<{HEADERSIZE}}' + msg
+        clientsocket.send(bytes(msg, "utf-8"))
