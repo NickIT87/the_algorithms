e9998053f8b1f310dff8966aa9137c0c0ec168a3
Nickolazz1 <elanir358@gmail.com>
2020-06-16 11:14:09 +0300

    3 less compleete (om)

--- kivyGUI/baseapp/main.py
+++ kivyGUI/baseapp/main.py
@@ -3,14 +3,13 @@ from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
 
 
+class Container(BoxLayout):
+    pass
+
+
 class MyApp(App):
     def build(self):
-        bl = BoxLayout()
-        button1 = Button(text='Hello')
-        button2 = Button(text='World')
-        bl.add_widget(button1)
-        bl.add_widget(button2)
-        return bl
+        return Container()
 
 
 if __name__ == "__main__":
