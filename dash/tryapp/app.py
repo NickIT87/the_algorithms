57cd45b412fa2f9dc247a826187df36969f8c456
Nickolazz1 <elanir358@gmail.com>
2020-06-24 20:49:55 +0300

    test app

--- dash/tryapp/app.py
+++ dash/tryapp/app.py
@@ -1,1 +1,34 @@
-# dash code add later...
+# -*- coding: utf-8 -*-
+
+# Загрузим необходимые пакеты
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+#  Объяснение данных строк пока опускается, будет объяснено далее
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
+
+    html.Div(children='''
+        Dash: A web application framework for Python.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure={
+            'data': [
+                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+            ],
+            'layout': {
+                'title': 'Dash Data Visualization'
+            }
+        }
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
