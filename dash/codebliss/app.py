bd53033a14397cd90c32129b0c65a1cb40f38e97
Nickolazz1 <elanir358@gmail.com>
2020-06-24 13:14:16 +0300

    dash review codebliss l1

---
+++ dash/codebliss/app.py
@@ -0,0 +1,13 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+
+app = dash.Dash()
+
+app.layout = html.Div(
+    html.H1(children="My dash project")
+)
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
