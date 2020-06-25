aa371eb34e08a2d26b4e7e398a701b7c4800bea9
Nickolazz1 <elanir358@gmail.com>
2020-06-25 14:06:16 +0300

    10 minutes to pandas

--- PandasTutorial/doctutor.py
+++ PandasTutorial/doctutor.py
@@ -2,12 +2,19 @@
 import numpy as np
 import pandas as pd
 
+
+# Creating a Series by passing a list of values,
+# letting pandas create a default integer index:
 s = pd.Series([1, 3, 5, np.nan, 6, 8])
-print(s)
+#print(s)
 
+
+#Creating a DataFrame by passing a NumPy array,
+# with a datetime index and labeled columns:
 dates = pd.date_range('20130101', periods=6)
-print(dates)
-
+#print(dates)
 df = pd.DataFrame(np.random.randn(6, 4), index=dates, columns=list('ABCD'))
-print(df)
+#print(df)
 
+
+# ... page 14
