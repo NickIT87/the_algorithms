72b93fa30812e1dd5ba0be304f92664eb60af47f
Nickolazz1 <elanir358@gmail.com>
2020-08-20 19:59:15 +0300

    sentdex kivy less 2 incompleete

--- kivyGUI/sentdexTut/kivyapp.py
+++ kivyGUI/sentdexTut/kivyapp.py
@@ -3,6 +3,8 @@ from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+import os
 
 
 #print(kivy.__version__)
@@ -14,17 +16,41 @@ class ConnectPage(GridLayout):
         super().__init__(**kwargs)
         self.cols = 2
 
+        if os.path.isfile("prev_details.txt"):
+            with open("prev_details.txt", "r") as f:
+                d = f.read().split(",")
+                prev_ip = d[0]
+                prev_port = d[1]
+                prev_username = d[2]
+        else:
+             prev_ip = "no data"
+             prev_port = "no data"
+             prev_username = "no data"
+
         self.add_widget(Label(text="IP:"))
-        self.ip = TextInput(multiline=False)
+        self.ip = TextInput(text=prev_ip, multiline=False)
         self.add_widget(self.ip)
 
         self.add_widget(Label(text="Port:"))
-        self.port = TextInput(multiline=False)
+        self.port = TextInput(text=prev_port, multiline=False)
         self.add_widget(self.port)
 
         self.add_widget(Label(text="Username:"))
-        self.username = TextInput(multiline=False)
+        self.username = TextInput(text=prev_username,multiline=False)
         self.add_widget(self.username)
+
+        self.join = Button(text="Join")
+        self.join.bind(on_press=self.join_button)
+        self.add_widget(Label(text="Connect here:"))
+        self.add_widget(self.join)
+
+    def join_button(self, instance):
+        port = self.port.text
+        ip = self.ip.text
+        username = self.username.text
+        print(f"Attempting to join {ip}:{port} as {username}")
+        with open("prev_details.txt", "w") as f:
+            f.write(f"{ip},{port},{username}")
 
 
 class EpicApp(App):
