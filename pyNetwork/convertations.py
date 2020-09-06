916465f6066aff78ab143146e6df5a0a1c73b272
Nickolazz1 <elanir358@gmail.com>
2020-09-06 19:00:06 +0300

    xor swap algorithm

--- pyNetwork/convertations.py
+++ pyNetwork/convertations.py
@@ -1,3 +1,4 @@
+# binary convertations
 bin_a = "01010101"
 
 print(int(bin_a, 2))
@@ -7,8 +8,19 @@ bin_b = bin(123)
 print(type(bin_b))
 print(int(bin_b, 2))
 
-
+# xor operation
 x = True
 y = False
 
 print((x ^ y) ^ y)
+
+# swap algorithm
+a = 2
+b = 8
+
+a ^= b
+b ^= a
+a ^= b
+
+print(a, b)
+# end 
