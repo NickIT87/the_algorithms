13ff797de2beaecccbc48f62e4e7749e9f3082c7
Nickolazz1 <elanir358@gmail.com>
2020-09-11 12:04:22 +0300

    some decorator test

--- automation/funcPerfomance/perfomance_test.py
+++ automation/funcPerfomance/perfomance_test.py
@@ -3,23 +3,34 @@
 from datetime import datetime
 
 
-def one():
-    start = datetime.now()
+def timeit(func):
+    def wrapper(*args, **kwargs):
+        start = datetime.now()
+        result = func(*args, **kwargs)
+        print(datetime.now() - start)
+        return result
+    return wrapper
+
+@timeit
+def one(n):
+    #start = datetime.now()
     l = []
-    for i in range(10**4):
+    #for i in range(10**4):
+    for i in range(n):
         if i % 2 == 0:
             l.append(i)
-    print(datetime.now() - start)
+    #print(datetime.now() - start)
     return l
 
-def two():
-    start = datetime.now()
-    l = [ x for x in range(10**4) if x % 2 == 0]
-    print(datetime.now() - start)
+@timeit
+def two(n):
+    #start = datetime.now()
+    l = [ x for x in range(n) if x % 2 == 0]
+    #print(datetime.now() - start)
     return l
 
-l1 = one()
-l2 = two()
+l1 = one(10000)
+l2 = two(10000)
 
 #print(l1)
 #print(l2)
