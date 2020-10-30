5ba44e1ad9d9904b6274e4688a473cb280b2cef6
Nickolazz1 <elanir358@gmail.com>
2020-10-30 16:06:23 +0200

    luxoft test

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
 #the algorithms
 
-print("hello world")
+d1 = {1: 'a1', 
+      2: {3: 'a3'}, 
+      5: 'a5'}
+
+
+d2 = {2: {4: 'a4'}, 
+      3: 'a3', 
+      5: 'five'}
+
+d0 = {}
+
+d0.update(d1)
+print(d0)
+
+print("d1 before: ", d1)
+d1.update(d2)
+d1.update(d0)
+d1.update(d2[2])
+print("d1 after: ", d1)
+
