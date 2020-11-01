b892ae36f77a3347d08a1ce6ec27de055d53535b
Nickolazz1 <elanir358@gmail.com>
2020-11-01 19:13:46 +0200

    less2, conways

--- digital_life/electronic_life.py
+++ digital_life/electronic_life.py
@@ -1,29 +1,34 @@
 # https://www.youtube.com/watch?v=GKe1aGQlKDY&list=PLryDJVmh-ww1OZnkZkzlaewDrhHy2Rli2
 import pygame
 import sys
+from game_window_class import *
 
 
 WIDTH, HEIGHT = 800, 800
-BACKGROUND = (42, 21, 13)
+BACKGROUND = (199, 199, 199)
 FPS = 60
 
 
 def get_events():
+    global running
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
 
 def update():
-    pass
+    game_window.update()
 
 
 def draw():
+    game_window.draw()
     window.fill(BACKGROUND)
+
 
 pygame.init()
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
+game_window = Game_window(window, 10, 10)
 
 running = True
 while running:
---
+++ digital_life/game_window_class.py
@@ -0,0 +1,19 @@
+import pygame
+
+
+vec = pygame.math.Vector2
+
+class Game_window:
+    def __init__(self, screen, x, y):
+        self.screen = screen
+        self.pos = vec(x, y)
+        self.width, self.height = 500, 500
+        self.image = pygame.Surface((self.width, self.height))
+        self.rect = self.image.get_rect()
+        
+    def update(self):
+        self.rect.topleft = self.pos
+
+    def draw(self):
+        self.image.fill((102, 102, 102))
+        self.screen.blit(self.image, (self.pos.x, self.pos.y))
