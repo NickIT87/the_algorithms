03e784b9636493c33d499b216677d41ef9a4b895
Nickolazz1 <elanir358@gmail.com>
2020-11-24 22:34:34 +0200

    think about locust

--- automation/locustio/locustfile.py
+++ automation/locustio/locustfile.py
@@ -1,8 +1,21 @@
-from locust import User, task, between
+import time
+from locust import HttpUser, task
 
-class MyUser(User):
+class QuickstartUser(HttpUser):
     @task
-    def my_task(self):
+    def hello_world(self):
+        self.client.get("https://martinschrader.pythonanywhere.com")
         print("executing my_task")
+        
 
-    wait_time = between(0.5, 10)
+    
+
+
+##from locust import User, task, between
+
+##class MyUser(User):
+##    @task
+##    def my_task(self):
+##        print("executing my_task")
+
+##    wait_time = between(0.5, 10)
