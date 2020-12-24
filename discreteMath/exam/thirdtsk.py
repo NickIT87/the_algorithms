b57dbbaf482dce27102b28062ba4f8a8ecbe6e98
Nickolazz1 <elanir358@gmail.com>
2020-12-24 09:40:14 +0200

    exam solution

---
+++ discreteMath/exam/thirdtsk.py
@@ -0,0 +1,16 @@
+
+data = [1, 3, 5, 7, 4, 2, 0, 4]
+
+def totalOddElem(data_array):
+    ansv = 0
+ 
+    for i in range(0, len(data_array)):
+        if i % 2 != 0:
+            print(data_array[i])
+            ansv = ansv + data_array[i]
+
+    return ansv
+
+
+if __name__ == "__main__":
+    print(totalOddElem(data))
