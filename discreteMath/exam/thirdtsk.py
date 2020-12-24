3d32eea6737d86894f112319d29debf8085f64d0
Nickolazz1 <elanir358@gmail.com>
2020-12-24 17:40:47 +0200

    think about refactor

--- discreteMath/exam/thirdtsk.py
+++ discreteMath/exam/thirdtsk.py
@@ -1,17 +1,20 @@
 
-data = [1, 3, 5, 7, 4, 2, 0, 4]
+data = [[1, 3, 5, 7],
+        [4, 2, 0, 4]]
 
 def totalOddElem(data_array):
     ansv = 0
  
     for i in range(0, len(data_array)):
-        if i % 2 != 0:
-            print(data_array[i])
-            ansv = ansv + data_array[i]
+        for j in range(0, len(data_array[i])):
+            if j % 2 != 0:
+                print(data_array[i][j])
+                ansv = ansv + data_array[i][j]
 
     return ansv
 
-debug_data = [1, 2, 3, 4]
+debug_data = [[1, 2, 3, 4],
+              [1, 2, 3, 4]]
 
 if __name__ == "__main__":
-    print(totalOddElem(debug_data))
+    print(totalOddElem(data))
