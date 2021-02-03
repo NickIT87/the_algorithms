d095c03374a80ad33ed565b937692bdc68d66e29
salamanka <elanir358@gmail.com>
2021-02-03 15:31:07 +0200

    Abstract factory pattern design

--- OOP/abstract_factory.py
+++ OOP/abstract_factory.py
@@ -7,6 +7,26 @@ class IChair(metaclass=ABCMeta):
         """The Chair Interface"""
 
 
+class SmallChair(IChair):
+    def __init__(self):
+        self.height = 60
+        self.width = 60
+        self.depth = 60
+
+    def get_dimensions(self):
+        return {"height": self.height, "width": self.width, "depth": self.depth}
+
+
+class MediumChair(IChair):
+    def __init__(self):
+        self.height = 70
+        self.width = 70
+        self.depth = 70
+
+    def get_dimensions(self):
+        return {"height": self.height, "width": self.width, "depth": self.depth}
+
+
 class BigChair(IChair):
     def __init__(self):
         self.height = 80
@@ -23,6 +43,10 @@ class ChairFactory():
         try:
             if chairtype == "BigChair":
                 return BigChair()
+            if chairtype == "MediumChair":
+                return MediumChair()
+            if chairtype == "SmallChair":
+                return SmallChair()
             raise AssertionError("Chair is not found")
         except AssertionError as _e:
             print(_e)
@@ -30,4 +54,8 @@ class ChairFactory():
 
 if __name__ == "__main__":
     CHAIR = ChairFactory.get_chair("BigChair")
-    print(CHAIR.get_dimensions())
+    print(f"{CHAIR.__class__} : {CHAIR.get_dimensions()}")
+    CHAIR = ChairFactory.get_chair("MediumChair")
+    print(f"{CHAIR.__class__} : {CHAIR.get_dimensions()}")
+    CHAIR = ChairFactory.get_chair("SmallChair")
+    print(f"{CHAIR.__class__} : {CHAIR.get_dimensions()}")
