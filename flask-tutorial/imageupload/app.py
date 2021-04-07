4c1e27953c7ab0ac17892177d1afe095b2602075
salamanka <elanir358@gmail.com>
2021-04-07 17:54:09 +0300

    image db flask

--- flask-tutorial/imageupload/app.py
+++ flask-tutorial/imageupload/app.py
@@ -1,6 +1,10 @@
-from flask import Flask
+from flask import Flask, request, render_template, Response
+from werkzeug.utils import secure_filename
+import base64
 
-from db import db_init
+from db import db_init, db
+from models import Img
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///img.db'
@@ -9,7 +13,37 @@ db_init(app)
 
 
 @app.route('/')
-def hello():
-    pass
+def index():
+    return render_template('index.html')
 
 
+@app.route('/upload', methods=['POST'])
+def upload():
+    pic = request.files['pic']
+    if not pic:
+        return 'No pic uploaded', 400
+
+    filename = secure_filename(pic.filename)
+    mimetype = pic.mimetype
+    
+    img = Img(img=pic.read(), mimetype=mimetype, name=filename)
+    db.session.add(img)
+    db.session.commit()
+
+    return 'Img has been uploaded!', 200
+
+
+@app.route('/<int:id>')
+def get_img(id):
+    img = Img.query.filter_by(id=id).first()
+    if not img:
+        return 'No img with that id', 404
+
+    return Response(img.img, mimetype=img.mimetype)
+
+
+@app.route('/imgview/<int:id>', methods=['GET'])
+def imgview(id):
+    img = Img.query.filter_by(id=id).first()
+    image = base64.b64encode(img.img).decode('ascii')
+    return render_template('imgview.html', image=image)
--- flask-tutorial/imageupload/models.py
+++ flask-tutorial/imageupload/models.py
@@ -3,7 +3,6 @@ from db import db
 
 class Img(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    img = db.Column()
-
-
-# https://www.youtube.com/watch?v=zMhmZ_ePGiM&t=6s
+    img = db.Column(db.Text, unique=True, nullable=False)
+    name = db.Column(db.Text, nullable=False)
+    mimetype = db.Column(db.Text, nullable=False)
