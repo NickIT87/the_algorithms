ff171161727fce7f8ad21fa5467b566aeac97e09
Nickolazz1 <elanir358@gmail.com>
2021-09-17 19:27:17 +0300

    armstrong numbers

---
+++ Algorithms/armstrong.py
@@ -0,0 +1,27 @@
+def count_numbers(nmb):
+    i = 0
+    while nmb > 0:
+        nmb //= 10
+        #print(nmb)
+        i+=1
+    return i
+
+def check_for_armstrong(nmb):
+    i = count_numbers(nmb)
+    #print(i)
+    armstrong = 0
+    while nmb > 0:
+        numeric = nmb % 10
+        nmb //= 10
+        armstrong += pow(numeric, i)
+    return armstrong
+
+
+#num = int(input("Enter number:"))
+num = 122
+s = check_for_armstrong(num)
+     
+if s == num:
+    print("Armstrong Number")
+else:
+    print("Not Armstrong Number")
