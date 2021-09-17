da16a8b2b7e8f628efbde7bb7c809d339a63f828
Nickolazz1 <elanir358@gmail.com>
2021-09-17 19:44:00 +0300

    strings revertion

--- StandartLibrary/string.py
+++ StandartLibrary/string.py
@@ -3,3 +3,24 @@
 s = 'the quick brown fox jumped over the lazy dog.'
 f = s.upper()
 print( f )
+
+string_ = "Revert me please:)"
+
+def method_1(string_):
+    return string_[::-1]
+    
+def method_2(string_):
+    mystr = ""
+    for i in string_:
+        mystr = i + mystr
+    return mystr
+    
+def method_3(string_):
+    if len(string_) == 0:
+        return string_
+    else:
+        return method_3(string_[1:]) + string_[0]
+    
+print("1 -->", method_1(string_))
+print("2 -->", method_2(string_))
+print("3 -->", method_3(string_))
