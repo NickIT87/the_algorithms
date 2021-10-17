8ea3f21283545f0decc0e3a11bf71a8198653354
Nickolazz1 <elanir358@gmail.com>
2021-10-17 19:07:06 +0300

    simple calc

--- GUI_TK/tutorials/codemy/calculator.py
+++ GUI_TK/tutorials/codemy/calculator.py
@@ -22,6 +22,8 @@ def button_clear():
 def button_add():
     first_number = e.get()
     global f_num
+    global math
+    math = "addition"
     f_num = int(first_number)
     e.delete(0, END)
 
@@ -29,7 +31,45 @@ def button_add():
 def button_equal():
     second_number = e.get()
     e.delete(0, END)
-    e.insert(0, f_num + int(second_number))
+    if math == "addition":
+        e.insert(0, f_num + int(second_number))
+    
+    if math == "subtraction":
+        e.insert(0, f_num - int(second_number))
+
+    if math == "multiplication":
+        e.insert(0, f_num * int(second_number))
+
+    if math == "division":
+        e.insert(0, f_num / int(second_number))
+
+
+def button_subtract():
+    first_number = e.get()
+    global f_num
+    global math
+    math = "subtraction"
+    f_num = int(first_number)
+    e.delete(0, END)
+
+
+def button_multiply():
+    first_number = e.get()
+    global f_num
+    global math
+    math = "multiplication"
+    f_num = int(first_number)
+    e.delete(0, END)
+
+
+def button_divide():
+    first_number = e.get()
+    global f_num
+    global math
+    math = "division"
+    f_num = int(first_number)
+    e.delete(0, END)
+
 
 # define buttons
 button_1 = Button(root, text="1", padx=40, pady=20, command=lambda: button_click(1))
@@ -45,6 +85,9 @@ button_0 = Button(root, text="0", padx=40, pady=20, command=lambda: button_click
 button_add = Button(root, text="+", padx=39, pady=20, command=button_add)
 button_equal = Button(root, text="=", padx=90, pady=20, command=button_equal)
 button_clear = Button(root, text="C", padx=91, pady=20, command=button_clear)
+button_subtract = Button(root, text="-", padx=41, pady=20, command=button_subtract)
+button_multiply = Button(root, text="*", padx=40, pady=20, command=button_multiply)
+button_divide = Button(root, text="/", padx=40, pady=20, command=button_divide)
 
 # Put the buttons on the screen
 button_1.grid(row=3,column=0)
@@ -63,6 +106,10 @@ button_0.grid(row=4,column=0)
 button_clear.grid(row=4, column=1, columnspan=2)
 button_add.grid(row=5,column=0)
 button_equal.grid(row=5,column=1, columnspan=2)
+
+button_subtract.grid(row=6, column=0)
+button_multiply.grid(row=6, column=1)
+button_divide.grid(row=6, column=2)
 
 
 root.mainloop()
