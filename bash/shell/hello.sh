944e979d0c18e672ef37a2736a2165164947cd55
Nickolazz1 <elanir358@gmail.com>
2021-12-31 17:09:35 +0200

    sample bash

---
+++ bash/shell/hello.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "Hi!"
+date
+NAME='Alexis'
+
+echo 'My name is' $NAME 
+# input data command
+read -p "Enter your name and age: " USRNAME AGE
+
+echo "Hello" $USRNAME $AGE
+
+if [ $AGE == "21" ]
+then
+ echo "your age is ok"
+elif [ $AGE -lt "18" ]
+then
+ echo "your age is illegal"
+elif [ $AGE -gt "18" ]
+then
+ echo "your age is legal"
+else
+echo "none of the age"
+fi
