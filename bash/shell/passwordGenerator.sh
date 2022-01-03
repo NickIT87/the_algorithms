888386bbb6ce7e4948bb1d1a2e1da87003cfe003
Nickolazz1 <elanir358@gmail.com>
2022-01-03 15:22:16 +0200

    openssl

---
+++ bash/shell/passwordGenerator.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "This is a simple password generator"
+echo "Please enter the length of the password:"
+read PASS_LENGTH
+
+for p in $(seq 1);
+do
+    openssl rand -base64 48 | cut -c1-$PASS_LENGTH
+done
