f60d34dc9d87382db299735d72ef02eb5c22112f
Nickolazz1 <elanir358@gmail.com>
2022-01-30 17:35:50 +0200

    haskell

---
+++ discreteMath/haskell/sq_eq.hs
@@ -0,0 +1,17 @@
+main :: IO ()
+
+main = 
+    let
+        a = 1
+        b = 2
+        c = 1
+        d = b ^ 2 - 4 * a * c
+    in
+        if d < 0 then
+            putStrLn "no solutions"
+        else
+            let
+                x1 = (- b - sqrt d) / (2 * a)
+                x2 = (- b + sqrt d) / (2 * a)
+            in
+                print (x1, x2)
