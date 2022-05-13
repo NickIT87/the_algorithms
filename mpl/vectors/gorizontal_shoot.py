2af2d1cee549468dba6e6fc7afd4d5024fda7cf9
Nickolazz1 <elanir358@gmail.com>
2022-05-13 17:26:00 +0300

    horizontal shoot

--- mpl/vectors/gorizontal_shoot.py
+++ mpl/vectors/gorizontal_shoot.py
@@ -1,12 +1,21 @@
 import matplotlib.patches as mpatches
 import matplotlib.pyplot as plt
 import numpy as np
-from random import randint
+from scipy.constants import g
+
+# condition
+V0 = 253
+t = 8
+alpha = 45
+
+#sind = lambda degrees: np.sin(np.deg2rad(degrees))
+#cosd = lambda degrees: np.cos(np.deg2rad(degrees))
+#print(sind(90)) # Output 1.0
 
 
-x=np.array([x+1 for x in range(10)])
-y=np.array([0 for x in range(10)])
-z=np.array([x+1 for x in range(10)])
+x=np.array([V0*x*np.cos(np.deg2rad(alpha)) for x in range(t)])
+y=np.array([0 for x in range(t)])
+z=np.array([V0*x*np.sin(np.deg2rad(alpha)) - (0.5*g*x)**2 for x in range(t)])
 
 
 fig=plt.figure()
@@ -20,9 +29,9 @@ axes.tick_params(axis='x', colors='r')
 axes.tick_params(axis='y', colors='g')
 axes.tick_params(axis='z', colors='b')
 
-axes.set_xlim([-100,100])
-axes.set_ylim([-100,100])
-axes.set_zlim([-100,100])
+axes.set_xlim([-10,1500])
+axes.set_ylim([-10,10])
+axes.set_zlim([-10,700])
 
 axes.set_title('3D line plot geeks for geeks', color='Black')
 
@@ -32,4 +41,6 @@ line=axes.plot3D(x,y,z,'red')
 red_patch = mpatches.Patch(color='red', label='The red data')
 axes.legend(handles=[red_patch])
 
+print(len(x))
+print(len(z))
 plt.show()
