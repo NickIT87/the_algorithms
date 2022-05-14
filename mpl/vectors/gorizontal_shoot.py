b7cd599add300c201f20d650df9ee94d48054a8c
Nickolazz1 <elanir358@gmail.com>
2022-05-14 16:39:00 +0300

    school example 3d mpl

--- mpl/vectors/gorizontal_shoot.py
+++ mpl/vectors/gorizontal_shoot.py
@@ -1,46 +1,59 @@
-import matplotlib.patches as mpatches
-import matplotlib.pyplot as plt
-import numpy as np
-from scipy.constants import g
+# Визуализация задачи "Бросок тела под углом к горизонту"
+# created by NickIT87
 
-# condition
-V0 = 253
-t = 8
-alpha = 45
+import matplotlib.patches as mpatches   # импорт mpl для отрисовки "легенды"
+import matplotlib.pyplot as plt         # программный объект для отрисовки графика
+import numpy as np                      # библиотека для работы с двумерными массивами
+from scipy.constants import g           # гравитационная постоянная
 
-#sind = lambda degrees: np.sin(np.deg2rad(degrees))
-#cosd = lambda degrees: np.cos(np.deg2rad(degrees))
-#print(sind(90)) # Output 1.0
-
-
-x=np.array([V0*x*np.cos(np.deg2rad(alpha)) for x in range(t)])
-y=np.array([0 for x in range(t)])
-z=np.array([V0*x*np.sin(np.deg2rad(alpha)) - (0.5*g*x)**2 for x in range(t)])
-
-
+# базовые настройки координатной сетки
 fig=plt.figure()
 axes = plt.axes(projection='3d')
 
-axes.set_xlabel("x", color='black')
-axes.set_ylabel("y", color='black')
-axes.set_zlabel("z", color='black')
+# обозначения меток и цветов координатных осей
+axes.set_xlabel("X [abscissus] - dist. m.", color='black')
+axes.set_ylabel("Y [ordinatus] - n/a", color='black')
+axes.set_zlabel("Z [applicata] - alt. m.", color='black')
+axes.tick_params(axis='x', colors='darkred')
+axes.tick_params(axis='y', colors='darkgreen')
+axes.tick_params(axis='z', colors='darkblue')
 
-axes.tick_params(axis='x', colors='r')
-axes.tick_params(axis='y', colors='g')
-axes.tick_params(axis='z', colors='b')
+# настройки шага сетки
+axes.set_xlim([0,12000])    # X
+axes.set_ylim([-5,5])       # Y
+axes.set_zlim([0,4000])     # Z
 
-axes.set_xlim([-10,1500])
-axes.set_ylim([-10,10])
-axes.set_zlim([-10,700])
+# Заголовок графика
+axes.set_title(
+    'Бросок тела под углом к горизонту, \n на примере ттх полковой гаубицы Д30', 
+    color='black'
+)
 
-axes.set_title('3D line plot geeks for geeks', color='Black')
+# задание ттх для полковой гаубицы Д30
+# начальная скорость
+V0 = 740            # ОФЗ 690 м/с  КМЛ 740 м/с
+# время
+t = 23              # 22 cек.
+# угол стрельбы
+alpha = 45          # 47 град.
 
-line=axes.plot3D(x,y,z,'red')
+# уравнения движения
+x=np.array(
+    [V0*dt*np.cos(np.deg2rad(alpha)) for dt in range(t)]
+)
+y=np.array(
+    [0 for dt in range(t)]  #Ось y = 0 (движение ветра не учитываем)
+)
+z=np.array(
+    [V0*dt*np.sin(np.deg2rad(alpha))-(0.5*g*dt)**2 for dt in range(t)]
+)
 
+# отрисовка параболы
+HEAT_projectile_line = axes.plot3D(x,y,z,'red')
 
-red_patch = mpatches.Patch(color='red', label='The red data')
+# отрисовка описания ТТХ, "легенда"
+red_patch = mpatches.Patch(color='red', label='HEAT projectile: V=740m/s; a=45deg.')
 axes.legend(handles=[red_patch])
 
-print(len(x))
-print(len(z))
+# отрисовка графика
 plt.show()
