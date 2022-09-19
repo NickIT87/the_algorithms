bc0a49447a2df475acf0654b1f30d21648e4b0ea
NickIT87 <elanir358@gmail.com>
2022-09-19 08:39:29 +0300

    cryptography worked fernet

--- cryptography/test.py
+++ cryptography/test.py
@@ -1,1 +1,24 @@
-# pass
+from cryptography.fernet import Fernet
+
+
+# key = Fernet.generate_key()
+# print(key)
+
+# file = open('key.key', 'wb')
+# file.write(key)
+# file.close()
+
+file = open('key.key', 'rb')
+f_key = file.read()
+file.close()
+print(f_key)
+
+fernet = Fernet(f_key)
+
+message = "secret message"
+
+encMessage = fernet.encrypt(message.encode('utf-8'))
+print(encMessage)
+
+decMessage = fernet.decrypt(encMessage).decode('utf-8')
+print(decMessage)
