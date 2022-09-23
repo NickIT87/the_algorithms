baa0578d5784da09da7e3e1fdae31414af22b953
NickIT87 <elanir358@gmail.com>
2022-09-23 18:10:15 +0300

    factorial alg

---
+++ Algorithms/factorial.py
@@ -0,0 +1,16 @@
+# https://youtu.be/fW_OS3LGB9Q
+# https://www.youtube.com/watch?v=fW_OS3LGB9Q&t=745s
+
+
+def recur_factorial(n):
+    if n <= 1:
+        return 1
+    else:
+        return n*recur_factorial(n-1)
+
+a = []
+
+for i in range(10):
+    a.append(recur_factorial(i))
+
+print(a)
