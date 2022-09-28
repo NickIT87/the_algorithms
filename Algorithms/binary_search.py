b9ff3ddbb2824dc630f186a7aa2e2c66e9f86afb
NickIT87 <elanir358@gmail.com>
2022-09-28 11:49:28 +0300

    binary search

---
+++ Algorithms/binary_search.py
@@ -0,0 +1,32 @@
+# https://youtu.be/DE-ye0t0oxE
+# https://www.youtube.com/watch?v=DE-ye0t0oxE
+# https://www.youtube.com/c/Telusko
+
+pos = -1
+
+def search(list, n):
+    l = 0
+    u = len(list) - l
+
+    while l < u:
+        mid = (l+u) // 2
+
+        if list[mid] == n:
+            globals()['pos'] = mid
+            return True
+        else:
+            if list[mid] < n:
+                l = mid+1
+            else:
+                u = mid-1
+    
+    return False
+
+
+list = [4, 7, 8, 12, 45, 99, 102, 702, 10987, 56666]
+n = 10987
+
+if search(list, n):
+    print("Found at ", pos+1)
+else:
+    print("Not Found")
