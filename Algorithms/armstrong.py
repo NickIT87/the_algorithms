ac7f466cae4c5e2a57ab6b7384d9150f56773d7c
NickIT87 <elanir358@gmail.com>
2022-11-14 14:11:02 +0200

    refactor

--- Algorithms/armstrong.py
+++ Algorithms/armstrong.py
@@ -8,25 +8,26 @@ def count_numbers(nmb: int) -> int:
 
 def check_for_armstrong(nmb: int) -> bool:
     num: int = nmb          # актуальне число
-    armstrong = 0           
+    ansver = 0           
     i = count_numbers(nmb)  # показник степеню
     
     while nmb > 0:  
         numeric = nmb % 10
         nmb //= 10
-        armstrong += pow(numeric, i)
+        ansver += numeric ** i
     
-    return armstrong == num
+    return ansver == num
 
 
-def isArmstrong(number):
+def isArmstrong(number: int) -> bool:
     return sum((int(digit) ** len(str(number)) for digit in str(number))) == number
 
 
 #num = int(input("Enter number:"))
 num = 153
 s = check_for_armstrong(num)
-     
+#s = isArmstrong(num)
+
 if s:
     print("Armstrong Number")
 else:
