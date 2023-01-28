d09530d37c1d6554e1fc0a24697fd2d59a40160b
NickIT87 <elanir358@gmail.com>
2023-01-28 16:03:25 +0200

    first graph manipulation

--- discreteMath/graphs/NetworkX/asp_test.py
+++ discreteMath/graphs/NetworkX/asp_test.py
@@ -11,6 +11,21 @@ def ap(C:tuple, L:tuple):
     """ build graph on pair AP """
     print(C[0])
 
+    G = nx.Graph()
+    
+    for i in range(1, len(C[0])-1):
+        print(i)
+        G.add_edge(i, i+1)
+        if i == len(C[0])-2:
+            G.add_edge(i+1, 1)
+        
+
+    print(G.nodes.data())
+    nx.draw(G, with_labels=True)
+    plt.show()
+    
+    print(G)
+
 
 def ak():
     """ get canonical pair AK """
--- discreteMath/graphs/NetworkX/simplegr.py
+++ discreteMath/graphs/NetworkX/simplegr.py
@@ -3,13 +3,17 @@ import matplotlib.pyplot as plt
 
 
 G = nx.Graph()
-G.add_node(2)
+G.add_node('A', data="123", label="A")
 G.add_node(5)
 G.add_edge(2,5)
 G.add_edge(4, 1)
-G.add_edges_from([(2,5),(1,3)])
+G.add_edges_from([("A",5),(1,3)])
 
 
 print(nx.info(G))
-nx.draw(G)
+print(G.nodes.data())
+
+print(G._node["A"])
+
+nx.draw(G, with_labels=True)
 plt.show()
