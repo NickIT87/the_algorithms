dcb70c3163ce97246d1c32e6e8014074ff609598
NickIT87 <elanir358@gmail.com>
2023-01-28 20:29:34 +0200

    basic operations

--- discreteMath/graphs/NetworkX/simplegr.py
+++ discreteMath/graphs/NetworkX/simplegr.py
@@ -3,17 +3,37 @@ import matplotlib.pyplot as plt
 
 
 G = nx.Graph()
-G.add_node('A', data="123", label="A")
-G.add_node(5)
-G.add_edge(2,5)
-G.add_edge(4, 1)
-G.add_edges_from([("A",5),(1,3)])
+
+G.add_node(0)
+G.add_node(1)
+G.add_node(2)
+G.add_node(3)
+G.add_node(4)
+
+labelsDict = {}
+labelsDict[0] = "0_1"
+labelsDict[1] = "1_5"
+labelsDict[2] = "2_3"
+labelsDict[3] = "3_5"
+labelsDict[4] = "4_2"
+
+G.add_edge(0, 1)
+G.add_edge(1, 2)
+G.add_edge(2, 3)
+G.add_edge(3, 4)
+G.add_edge(4, 0)
+
+# G.add_node('A', data="123", label="A")
+# G.add_node(5)
+# G.add_edge(2,5)
+# G.add_edge(4, 1)
+# G.add_edges_from([("A",5),(1,3)])
 
 
-print(nx.info(G))
-print(G.nodes.data())
+# print(nx.info(G))
+# print(G.nodes.data())
 
-print(G._node["A"])
+# print(G._node["A"])
 
-nx.draw(G, with_labels=True)
+nx.draw(G, labels=labelsDict, with_labels=True)
 plt.show()
