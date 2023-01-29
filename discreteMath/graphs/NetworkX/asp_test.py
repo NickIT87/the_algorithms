9551ac73d09e44a24eb7e6038b697c5c625f184a
NickIT87 <elanir358@gmail.com>
2023-01-30 00:01:17 +0200

    need to implement reduction alg

--- discreteMath/graphs/NetworkX/asp_test.py
+++ discreteMath/graphs/NetworkX/asp_test.py
@@ -12,24 +12,40 @@ def ar():
 
 def ap(C:tuple, L:tuple):
     """ build graph on pair AP """
-    print(C[0])
-
-    G = nx.Graph()
     
-    for i in range(0, len(C[0])-2):
-        print(i)
-        G.add_edge(i, i+1)
-        if i == len(C[0])-3:
-            G.add_edge(i+1, 0)
-        
+    # STEP 0
+    G = nx.Graph()
+    G.add_node(0, label=C[0][0], path=C[0][0])
     labelsDict = {}
-    labelsDict[0] = "0_1"
-    labelsDict[1] = "1_5"
-    labelsDict[2] = "2_3"
-    labelsDict[3] = "3_5"
-    labelsDict[4] = "4_2"
-
+    labelsDict[0] = G.nodes[0]['label']         # xi v0=x'
+    
+    # STEP 1
+    # for loop ... in progress ...    
+    for i in range(1, len(C[0])-1):
+        G.add_node(i, label=C[0][i], path=None)
+        labelsDict[i] = C[0][i]
+        G.add_edge(i-1, i)
+        if (i == len(C[0])-2):
+            G.add_edge(i, 0)
+    #ar(G)
+    
     print(G.nodes.data())
+    
+# =============================================================================
+#     for i in range(0, len(C[0])-2):
+#         print(i)
+#         G.add_edge(i, i+1)
+#         if i == len(C[0])-3:
+#             G.add_edge(i+1, 0)
+# =============================================================================
+
+# =============================================================================
+#     print(G.nodes.data())
+#     print(G.nodes)
+#     print(G._node[0])
+#     print(G._node)
+# =============================================================================
+    
     nx.draw(
         G, 
         labels=labelsDict, 
@@ -40,8 +56,6 @@ def ap(C:tuple, L:tuple):
         width = 4
     )
     plt.show()
-    
-    print(G)
 
 
 def ak():
