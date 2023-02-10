7b7df3891cc20a61b86c0c9c129930de5e1420eb
NickIT87 <elanir358@gmail.com>
2023-02-10 17:18:58 +0200

    lab

--- cryptography/caesar.py
+++ cryptography/caesar.py
@@ -1,13 +1,43 @@
-alpha = ' abcdefghijklmnopqrstuvwxyz'
-n = int(input())
-s = input().strip()
-res = ''
-for c in s:
-    res += alpha[(alpha.index(c) + n) % len(alpha)]
-print('Result: "' + res + '"')
+EN = ' abcdefghijklmnopqrstuvwxyz'
+UA = ' абвгґдеєжзиіїйклмнопрстуфхцчшщьюя'
 
-dcp = ''
-for i in res:
-    dcp += alpha[(alpha.index(i) + len(alpha) - (n % len(alpha))) % len(alpha)]
+# y = (x + k) mod n
+# x = (y + n - (k mod n)) mod n
 
-print(dcp)
+# x - символ відкритого тексту, 
+# y - символ шифрованого тексту, 
+# n - потужність алфавіту, 
+# k - ключ.
+
+def caesar(text:str, k:int, ABC:str, dec:int=0) -> str:
+    res: str = ""
+    text = text.lower()
+    k = abs(k)
+    n = len(ABC)
+
+    if dec:
+        for y in text:
+            res += ABC[(ABC.index(y) + n - (k % n)) % n]
+    else:
+        for x in text:
+            res += ABC[(ABC.index(x) + k) % n]
+
+    return res
+
+key = 12 #int(input())
+user_text = "abc" #input().strip()
+
+print(caesar(user_text, key, EN))
+print(caesar(caesar(user_text, key, EN), key, EN, 1))
+
+
+# res = ''
+# for c in user_text:
+#     res += EN[(EN.index(c) + key) % len(EN)]
+# print('Result: "' + res + '"')
+
+# dcp = ''
+# for i in res:
+#     dcp += EN[(EN.index(i) + len(EN) - (key % len(EN))) % len(EN)]
+
+# print(dcp)
---
+++ cryptography/lab1/main.py
@@ -0,0 +1,37 @@
+class CAESAR(object):
+    def __init__(self, key:int, alpha:str='EN'):
+        if alpha == 'EN':
+            self.ABC = ' abcdefghijklmnopqrstuvwxyz'
+        else:
+            self.ABC = ' абвгґдеєжзиіїйклмнопрстуфхцчшщьюя'
+        
+        if key <= 0 or key >= len(self.ABC):
+            raise ValueError('key must be greater than 0 and less than ABC length')
+        
+        self.__k:int = key
+        self.n = len(self.ABC)
+
+    def enc(self, text:str) -> str:
+        res = ''
+        for x in text.lower():
+            res += self.ABC[(self.ABC.index(x) + self.__k) % self.n]
+        return res
+
+    def dec(self, text:str) -> str:
+        res = ''
+        for y in text.lower():
+            res += self.ABC[(self.ABC.index(y) + self.n - (self.__k % self.n)) % self.n]
+        return res
+
+    def __str__(self) -> str:
+        return "Dev: Prytula M.I. 124m-22-1"
+
+
+def main():
+    cipher = CAESAR(key=12, alpha="UA")
+    print(cipher.enc("фґв"))
+    print(cipher.dec("вмк"))
+
+
+if __name__ == "__main__":
+    main()
