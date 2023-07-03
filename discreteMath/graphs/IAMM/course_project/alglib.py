ce90372bce9b95effb4dcc1bfc24e7cd07ac5001
NickIT87 <elanir358@gmail.com>
2023-07-03 12:34:14 +0300

    think about project structure

---
+++ discreteMath/graphs/IAMM/course_project/alglib.py
@@ -0,0 +1,52 @@
+""" IAMM - Graphs - ASP Work """
+
+import networkx as nx               # type: ignore
+from typing import Tuple, List, Union
+
+
+def ak_pair(graph: nx.Graph) -> Union[Tuple[List[str], List[str]], int, str]:
+    """ get canonical pair AK """
+
+    if len(graph.nodes) == 0:
+        return 0
+    elif len(graph.nodes) == 1:
+        try:
+            return graph.nodes[0]['label']
+        except KeyError:
+            return list(graph.nodes)[0]
+
+    root = list(graph.nodes)[0]
+    sigma_g: list = []
+    lambda_g: list = []
+    reachability_basis: dict = dict()
+    
+    # Find reachability basis in the graph and fill lambda_g
+    for node in graph.nodes:
+        node_path_id = nx.shortest_path(graph, source=root, target=node)
+        node_path_labels = [graph.nodes[id]['label'] for id in node_path_id]
+        reachability_basis[''.join(node_path_labels)] = node_path_id
+        if graph.degree(node) == 1 and node != root:
+            lambda_g.append(''.join(node_path_labels))
+
+    # create ni var as reachibility basis list without lambda_g values
+    ni = [w for w in reachability_basis.keys() if w not in lambda_g]
+    ni.pop(root)
+
+    # Find cycles by ni and fill sigma_g
+    for i, p in enumerate(ni):
+        for q in ni[i+1:]:
+            print(p, q)
+            if p not in q[:len(p)]:
+                if graph.has_edge(
+                    reachability_basis[p][-1], 
+                    reachability_basis[q][-1]
+                ) is False:
+                    continue
+                pqr = p + q[::-1]
+                qpr = q + p[::-1]
+                if pqr < qpr:
+                    sigma_g.append(pqr)
+                else:
+                    sigma_g.append(qpr)
+   
+    return (sigma_g, lambda_g)
---
+++ discreteMath/graphs/IAMM/course_project/data.py
@@ -0,0 +1,29 @@
+import networkx as nx               # type: ignore
+
+
+# TEST DATA
+G = nx.Graph()
+
+G.add_node(0, label='0', color="lightblue")
+G.add_node(1, label='1', color="lightblue")
+G.add_node(2, label='2', color="lightblue")
+G.add_node(3, label='3', color="lightblue")
+G.add_node(4, label='4', color="lightblue")
+G.add_node(5, label='5', color="lightblue")
+G.add_node(6, label='6', color="lightblue")
+
+G.add_edge(0, 1)
+G.add_edge(1, 2)
+G.add_edge(1, 3)
+G.add_edge(1, 4)
+G.add_edge(1, 5)
+G.add_edge(2, 3)
+G.add_edge(2, 4)
+G.add_edge(2, 5)
+G.add_edge(2, 6)
+G.add_edge(3, 4)
+G.add_edge(3, 5)
+G.add_edge(3, 6)
+G.add_edge(4, 5)
+G.add_edge(4, 6)
+G.add_edge(5, 6)
