c6a59196c872b8d3dec8142e08bf4d4fd1ebc1ea
NickIT87 <elanir358@gmail.com>
2023-08-19 13:01:57 +0300

    dash netowrkx plot graph

--- dash/graph.py
+++ dash/graph.py
@@ -2,94 +2,72 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
-import plotly.graph_objects as go
 import networkx as nx
+import plotly.graph_objs as go
 
-# Plotly figure
-def networkGraph(EGDE_VAR):
 
-    edges = [[EGDE_VAR, 'B'], ['B', 'C'], ['B', 'D']]
-    G = nx.Graph()
-    G.add_edges_from(edges)
-    pos = nx.spring_layout(G)
+app = dash.Dash(__name__)
+G = nx.karate_club_graph()
+app.layout = html.Div([
+    dcc.Graph(id='network-graph')
+])
 
-    # edges trace
-    edge_x = []
-    edge_y = []
+
+@app.callback(
+    Output('network-graph', 'figure'),
+    Input('network-graph', 'relayoutData')  # This can be replaced with other inputs
+)
+def update_graph(relayoutData):
+    # Create Plotly figure from NetworkX graph
+    pos = nx.spring_layout(G)  # Layout algorithm (e.g., spring_layout, circular_layout)
+    edge_trace = go.Scatter(
+        x=[],
+        y=[],
+        line=dict(width=0.5, color='#888'),
+        hoverinfo='none',
+        mode='lines')
+
     for edge in G.edges():
         x0, y0 = pos[edge[0]]
         x1, y1 = pos[edge[1]]
-        edge_x.append(x0)
-        edge_x.append(x1)
-        edge_x.append(None)
-        edge_y.append(y0)
-        edge_y.append(y1)
-        edge_y.append(None)
-
-    edge_trace = go.Scatter(
-        x=edge_x, y=edge_y,
-        line=dict(color='black', width=1),
-        hoverinfo='none',
-        showlegend=False,
-        mode='lines')
-
-    # nodes trace
-    node_x = []
-    node_y = []
-    text = []
-    for node in G.nodes():
-        x, y = pos[node]
-        node_x.append(x)
-        node_y.append(y)
-        text.append(node)
+        edge_trace['x'] += (x0, x1, None)
+        edge_trace['y'] += (y0, y1, None)
 
     node_trace = go.Scatter(
-        x=node_x, y=node_y, text=text,
-        mode='markers+text',
-        showlegend=False,
-        hoverinfo='none',
+        x=[],
+        y=[],
+        text=[],
+        mode='markers',
+        hoverinfo='text',
         marker=dict(
-            color='pink',
-            size=50,
-            line=dict(color='black', width=1)))
+            showscale=True,
+            colorscale='YlGnBu',
+            size=10,
+            colorbar=dict(
+                thickness=15,
+                title='Node Connections',
+                xanchor='left',
+                titleside='right'
+            )
+        )
+    )
 
-    # layout
-    layout = dict(plot_bgcolor='white',
-                  paper_bgcolor='white',
-                  margin=dict(t=10, b=10, l=10, r=10, pad=0),
-                  xaxis=dict(linecolor='black',
-                             showgrid=False,
-                             showticklabels=False,
-                             mirror=True),
-                  yaxis=dict(linecolor='black',
-                             showgrid=False,
-                             showticklabels=False,
-                             mirror=True))
+    for node in G.nodes():
+        x, y = pos[node]
+        node_trace['x'] += (x,)
+        node_trace['y'] += (y,)
+        node_trace['text'] += (f'Node {node}',)
 
-    # figure
-    fig = go.Figure(data=[edge_trace, node_trace], layout=layout)
+    fig = go.Figure(data=[edge_trace, node_trace],
+                    layout=go.Layout(
+                        showlegend=False,
+                        hovermode='closest',
+                        margin=dict(b=0, l=0, r=0, t=0),
+                        height=800
+                    ))
 
     return fig
 
-# Dash app
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-app.title = 'Dash Networkx'
-
-app.layout = html.Div([
-        html.I('Write your EDGE_VAR'),
-        html.Br(),
-        dcc.Input(id='EGDE_VAR', type='text', value='K', debounce=True),
-        dcc.Graph(id='my-graph'),
-    ]
-)
-
-@app.callback(
-    Output('my-graph', 'figure'),
-    [Input('EGDE_VAR', 'value')],
-)
-def update_output(EGDE_VAR):
-    return networkGraph(EGDE_VAR)
 
 if __name__ == '__main__':
-    app.run_server(debug=True, host='0.0.0.0', port=1234)
+    app.run_server(debug=True)
